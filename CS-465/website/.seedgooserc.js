12536e587a9841a5ed7ec2b0025b41ab464fd017
Samuel Bailey <samuel.bailey@life.church>
2021-07-20 21:05:20 -0500

    Module 4 assignment

---
+++ CS-465/website/.seedgooserc.js
@@ -0,0 +1,8 @@
+const { model } = require("mongoose");
+
+model.exports = {
+    modelBaseDirectory: 'app_server/models',
+    models: ['*.js', '!db.js'],
+    data: 'data', 
+    db: 'mongodb://localhost:27017/travlr'
+};
