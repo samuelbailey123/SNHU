35496e2a14441da53d17b161c59a2ddfb28f052f
Samuel Bailey <samuel.bailey@life.church>
2021-10-16 09:43:03 -0700

    Restructure

---
+++ Undergrad/CS-330/CS-330 Windows/pyramid.cpp
@@ -0,0 +1,28 @@
+	//1.0f, 1.0f, 1.0f, 1.0f, << white color 
+        //1.0f, 0.0f, 0.0f, 1.0f, << red color 
+        //0.0f, 0.0f, 1.0f, 1.0f, << blue color 
+        //0.0f, 1.0f, 0.0f, 1.0f, << green color
+        //0.0f, 1.0f, 1.0f, 1.0f, << cyan color 
+        //1.0f, 1.0f, 0.0f, 1.0f, << yellow color
+        //1.0f, 0.0f, 2.0f, 1.0f, << purple color 
+
+        //triangle 1
+        // main triangle / middle / right side
+        0.0f, 1.0f, .0f,         1.0f, 1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,     1.0f, 0.0f, 2.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,      1.0f, 1.0f, 0.0f, 1.0f,
+
+        //other triangle
+        0.0f, 1.0f, 0.0f,       0.0f, 0.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,    0.0f, 0.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,     0.0f, 0.0f, 1.0f, 1.0f,
+
+        //left side
+        0.0f, 1.0f, 0.0f,       0.0f, 1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,    0.0f, 1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,     1.0f, 0.0f, 2.0f, 1.0f,
+
+        //other triangle
+        0.0f, 1.0f, 0.0f,       0.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,     0.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,      0.0f, 1.0f, 1.0f, 1.0f,
