adc57b31c692e8b6f2ec8f0922834e184d889ff6
Samuel Bailey <samuel.bailey@life.church>
2021-10-16 09:23:33 -0700

    Added project to CS-470

---
+++ CS-470-T1055/project/lafs-api/server/datasources.development.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mongodb: {
+    connector: 'mongodb',
+    hostname: '10.0.0.180',
+    port: 27017,
+    user: '',
+    password: '',
+    database: 'lafs',
+    url: process.env.DB_URL,
+  },
+};
---
+++ CS-470-T1055/project/lafs-web-2/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  api_url: 'https://jz0e12fhs3.execute-api.us-east-2.amazonaws.com/api'
+};
